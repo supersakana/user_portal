eb62b87cb357901d1ae11ed84e80fc53476f3675
supersakana <zacwilliamson@icloud.com>
2023-08-15 16:34:33 -0600

    installed and set up more server dependencies

--- server/index.js
+++ server/index.js
@@ -1,6 +1,31 @@
 const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
 const app = express()
 
 app.listen(4000, () => {
     console.log("Server started on PORT 4000")
 })
+
+mongoose.connect("mongodb://localhost:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log("DB Connection Successful")
+})
+.catch((error) => {
+    console.log(error.message)
+})
+// set up new db and make sure this is connected propely in production
+
+app.use(
+    cors({
+        origin: ["http://localhost:3000"],
+        method: ["GET", "POST"],
+        credentials: true
+    })
+)
+// ^^^ needs to be edited based on env
+
+app.use(express.json())
