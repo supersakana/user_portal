4e7073028962356231cad895c518ba50322e1074
supersakana <zacwilliamson@icloud.com>
2023-08-16 12:15:03 -0600

    set up basic register page

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,5 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom"
+import { ToastContainer, toast } from "react-toastify";
 
 export default function PageName() {
-  return <div>Register</div>;
+    const [values, setValues] = useState({
+        email: "",
+        password: ""
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+      };
+
+  return(
+    <div className="container">
+      <h2>Register Account</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            onChange={(e) =>
+              setValues({ ...values, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            onChange={(e) =>
+              setValues({ ...values, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+
+        <span>
+          Already have an account? <Link to="/login">Login</Link>
+        </span>
+        
+      </form>
+      <ToastContainer />
+    </div>
+
+  )
 }
