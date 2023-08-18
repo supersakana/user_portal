a1618f74029969ec5662ee1a8ac649944e44d70e
supersakana <zacwilliamson@icloud.com>
2023-08-18 14:23:03 -0600

    created simple secret page

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -26,10 +26,11 @@ export default function Register() {
                 
                 if(data){
                   if(data.errors){
+                    
                     const { email, password } = data.errors
                     if(email) generateError(email)
                     else if (password) generateError(password)
-                    
+                  
                   } else {
                     navigate("/")
                   }
--- public/src/pages/Secret.jsx
+++ public/src/pages/Secret.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom"
 
 export default function Secret() {
-  return <div>Secret</div>;
+  const navigate = useNavigate()
+
+  const logOut = () => {
+    navigate("/login")
+  }
+
+  return(
+    <div>
+      <h1>Secret Page</h1>
+      <button onClick={logOut}>Log out</button>
+    </div>
+  )
 }
