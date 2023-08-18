7e3d7df3b339138b0c09d046cf3b98294f596c15
supersakana <zacwilliamson@icloud.com>
2023-08-18 14:44:54 -0600

    added login functionality for our UserModel

--- server/Controllers/AuthControllers.js
+++ server/Controllers/AuthControllers.js
@@ -42,4 +42,23 @@ module.exports.register = async (req, res, next) => {
     }
 };
 
-module.exports.login = async (req, res, next) => {};
+module.exports.login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+        const user = await UserModel.login(email, password)
+        const token = createToken(user._id)
+
+        res.cookie("jwt", token, {
+            withCredentials: true,
+            httpOnly: false,
+            maxAge: maxAge * 1000
+        })
+
+        res.status(200).json( {user:user._id, created:true })
+
+    } catch(error) {
+        const errors = handleErrors(error)
+        res.json({ errors, created: false })
+
+    }
+};
--- server/Models/UserModel.js
+++ server/Models/UserModel.js
@@ -18,4 +18,16 @@ userSchema.pre("save", async function (next) {
     this.password = await bcrypt.hash(this.password, salt)
 })
 
+userSchema.statics.login = async function (email, password) {
+    const user = await this.findOne({ email })
+    if(user) {
+        const auth = await bcrypt.compare(password, user.password)
+        if(auth) {
+            return user
+        }
+        throw Error("Incorrect password")
+    }
+    throw Error("Incorrect email")
+}
+
 module.exports = mongoose.model("Users", userSchema)
