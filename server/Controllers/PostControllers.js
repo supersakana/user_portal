8d88c7ddbf69c0c90376c0d0d16608a09e008175
supersakana <zacwilliamson@icloud.com>
2023-08-21 19:29:53 -0600

    user is updated with new post (saved to the db)

--- server/Controllers/PostControllers.js
+++ server/Controllers/PostControllers.js
@@ -1,11 +1,19 @@
 const PostModel = require("../Models/PostModel");
+const User = require("../Models/UserModel")
 
 module.exports.post = async (req, res, next) => {
     try {
         const { title, body, user } = req.body
+        
         const post = await PostModel.create({ title, body, user })
+        const currentUser = await User.findById(user._id)
+
+        currentUser.posts = [...currentUser.posts, post]
+        currentUser.save()
+
+        console.log(currentUser.posts)
        
-        res.status(201).json( { post: post, created:true })
+        res.status(201).json({ created:true })
 
     } catch(error) {
         res.json({ error, created: false })
